eda1b7a6bbec5534e64959064a1485ec64d1a23f
Rahims609 <rahimboss609@gmail.com>
2022-04-06 17:11:44 +0600

    google popup added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import app from './firebase.init';
-import {getAuth} from 'firebase/auth'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 
 const auth = getAuth(app);
 
 function App() {
+
+  const provider = new GoogleAuthProvider();
+  const handleGoogleSingIn = () => {
+      signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user ;
+        console.log(user)
+      }).catch(error => {
+        console.log(error)
+      })
+  }
+
   return (
     <div className="App">
-      
+      <button onClick={handleGoogleSingIn}>Google Sing In </button>
     </div>
   );
 }

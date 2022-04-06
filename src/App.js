477ad6dad886a977bd09498711c2d659c713d46a
Rahim2725 <info.abdurrahim12@gmail.com>
2022-04-07 05:50:28 +0600

    sing out done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+import { useState } from 'react';
 
 const auth = getAuth(app);
 
 function App() {
 
   const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState([])
+
   const handleGoogleSingIn = () => {
-      signInWithPopup(auth, provider)
+    signInWithPopup(auth, provider)
       .then((result) => {
-        const user = result.user ;
-        console.log(user)
+        const user = result.user;
+        setUser(user);
+        // console.log(user);
       }).catch(error => {
-        console.log(error)
+        // console.log(error);
       })
   }
 
+  const handleGoogleSingOut = () => {
+    signOut(auth).then(() => {
+      // console.log('sing out successful');
+      setUser([]);
+    }).catch(error => {
+      // console.error('problem');
+    })
+  }
   return (
     <div className="App">
+
+      { 
+      user.email ? <button onClick={handleGoogleSingOut}>Sing Out</button> :   
       <button onClick={handleGoogleSingIn}>Google Sing In </button>
+
+      }
+
+      <h2>Name: {user.displayName}</h2>
+      <p>I know your Email Address : {user.email}</p>
+      <img src={user.photoURL} alt="" />
+
     </div>
   );
 }
